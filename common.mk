72f09e1cf57ab0098240a1ebba2caaf35dce2f8a
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-06-30 10:16:51 +0000

    sm6225-common: Configure WiFi
    Change-Id: Ie233ea22aca3924c9acbbd67b06505f5dc48f2c1

--- common.mk
+++ common.mk
@@ -167,5 +167,28 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi-service \
+    hostapd \
+	hostapd_cli \
+    libwifi-hal-qcom \
+	wpa_cli \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# WiFi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
+
+# WiFi config files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+PRODUCT_VENDOR_MOVE_ENABLED := true
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/sm6225-common/sm6225-common-vendor.mk)
