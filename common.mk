e138bd282e7f6c100ecdbd1afb844fd4fc596a9d
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-07-03 15:58:18 +0000

    sm6225-common: Build QTI Power HAL
    Change-Id: I2e04997c9236d1ffa384d445e758406d3af17f0f

--- common.mk
+++ common.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     WifiResTarget
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
